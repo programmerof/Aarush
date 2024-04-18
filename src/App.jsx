49487c90065b6221cbde3aa73b7dd13830db4346
aarushinternational294 <166803505+aarushinternational294@users.noreply.github.com>
2024-04-18 23:05:49 +0545

    made in react

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,25 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import CardComponent from './pages/serviceStack';
 import StudyCountry from './pages/study';
+import Service from './pages/Service';
+import Footer from './pages/footer';
+import Header from './pages/Header';
+import Hero from './pages/Hero';
 
 const App = () => {
   return (
     <Router>
+      {/* Put components in here */}
+      <Header />
+      <Hero />
+      <Service />
+      <StudyCountry />
+      <CardComponent />
+      <Footer />
 
       <Routes>
-        <Route path="/" element={<CardComponent />} />
-        <Route path="/study" element={<StudyCountry />} />
+        {/* <Route path="/" element={<CardComponent />} />
+        <Route path="/study" element={<StudyCountry />} /> */}
 
 
       </Routes>
--- src/pages/serviceStack.jsx
+++ src/pages/serviceStack.jsx
@@ -18,7 +18,7 @@ const CardComponent = () => {
         </div>
         <div className="flex justify-center">
           <p className="text-black font-semibold border rounded-md md:w-6/6 bg-gradient-to-b from-blue-50 to-white p-1">
-            Aarush International Education Limited, your trusted partner for
+            Aarush International Education Private Ltd, your trusted partner for
             navigating the complexities of studying abroad.
           </p>
         </div>
